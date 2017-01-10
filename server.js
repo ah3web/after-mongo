848e93c22c7a687ea5233884e1f30f8242dcb388
halorin <halorin@gmail.com>
2017-01-10 02:10:05 +0000

    More like it!

--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
 app.listen(8080);
 console.log('Server up');
+
+app.get('/', function(req, res){
+    
+   res.send('Moo'); 
+   
+});
+
+
+
+app.get('/:time', function(req, res){
+   
+   var time = req.params.time;
+   var timeResponse;
+   
+
+   if(/^\d{8,}$/.test(req.params.time)) {
+        
+        timeResponse = moment(req.params.time, "X");
+        
+    } else {
+        
+        timeResponse = moment(req.params.time, "MMMM D, YYYY");
+        
+    }
+
+
+    if ( timeResponse.isValid() ){
+
+        res.json({
+            unix: timeResponse.format("X"),
+            natural: timeResponse.format("MMMM D, YYYY")
+        });        
+        
+    } else {
+        
+        res.json({
+            
+            unix: null,
+            natural: null
+            
+        })
+        
+    }
+
+   
+
+    
+});
 
