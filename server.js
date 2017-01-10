ca3ee8ce2c095633ac9477477b3a318341cad37d
halorin <halorin@gmail.com>
2017-01-10 01:14:58 +0000

    Bleh

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
-var express = require('express');
+var server = require('express');
+var app = server();
+var moment = require('moment');
+var fs = require('fs');
+var path = require('path');
 
-var app = express();
+var port = process.env.PORT || 3500;
 
-app.get('/', function(req, res){
-    res.send('Hello World!');
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
 });
 
-app.listen(8080, function(){
-    console.log('Example server is go');
-})
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
